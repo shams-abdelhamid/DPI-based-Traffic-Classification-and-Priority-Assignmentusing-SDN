32c4e5420ce14efedff661068db0e95b408409ec
zeina <zeina.ayman@hotmail.com>
2022-01-03 19:58:15 +0200

    added comments

--- topology.py
+++ topology.py
@@ -1,13 +1,19 @@
 from mininet.TOPO import TOPO
 from mininet.net import Mininet
+from mininet.node import RemoteController,OVSSwitch
+from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 
 class Topology(Topo):
+    print("Custom Topology Example.")
     def __init__(self,k=2):
         Topo.__init(self)
+        print "Adding Switch"
         switch = self.addSwitch('s1')
         for x in range(k):
+            print "Adding Hosts"
             host = self.addHost('h%s' % (x + 1))
+            print("Link host and switch.")
             self.addLink(host,switch)
 
 def performTest():
