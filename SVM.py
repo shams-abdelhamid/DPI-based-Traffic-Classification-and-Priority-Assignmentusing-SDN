d3dc83721f8f97b4a31927bdaa83be8ebf32a3f2
NayraMawardyy <96235686+NayraMawardyy@users.noreply.github.com>
2022-01-03 21:58:42 +0200

    Add files via upload

---
+++ SVM.py
@@ -0,0 +1,13 @@
+from sklearn import svm
+from sklearn import metrics
+from preprocessing import X_train,y_train,X_test,y_test
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
