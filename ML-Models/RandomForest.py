33e2d2cbc9e0888a2a914fcca0bca039320d0205
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2022-01-03 20:56:19 +0200

    random forest Algo

---
+++ ML-Models/RandomForest.py
@@ -0,0 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from preprocessing import X_train,y_train,X_test,y_test
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
